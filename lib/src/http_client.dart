f6b8c7771efa16eca3cacecc64cff8a686fbf0c9
Danny Vaca <danny270793@icloud.com>
2024-09-08 22:39:57 -0500

    upload/download

--- lib/src/http_client.dart
+++ lib/src/http_client.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
 import 'package:jmixclientv1/src/exceptions/invalid_http_request_exception.dart';
@@ -113,6 +114,60 @@ class HttpClient {
         invalidResponse: InvalidResponse(
             statusCode: response.statusCode,
             body: response.body,
+            headers: response.headers,
+            reasonPhrase: response.reasonPhrase));
+  }
+
+  Future<Uint8List> download(
+      {required String url, required Map<String, String> headers}) async {
+    var request = http.Request('GET', Uri.parse(url));
+    request.headers.addAll(headers);
+
+    final http.StreamedResponse response = await request.send();
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      final Uint8List bytes = await response.stream.toBytes();
+      final buffer = bytes.buffer;
+      return buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
+    }
+
+    throw InvalidHttpRequestException(
+        invalidRequest: InvalidRequest(
+            uri: Uri.parse(url), method: 'DOWNLOAD', headers: headers),
+        invalidResponse: InvalidResponse(
+            statusCode: response.statusCode,
+            body: "",
+            headers: response.headers,
+            reasonPhrase: response.reasonPhrase));
+  }
+
+  Future<String> upload(
+      {required String url,
+      required Map<String, String> headers,
+      Map<String, dynamic>? query,
+      required file}) async {
+    final String queryParsed =
+        query != null ? Uri(queryParameters: query).query : '';
+    final Uri uri = Uri.parse("$url?$queryParsed");
+
+    var request = http.MultipartRequest('POST', uri);
+    request.fields.addAll({
+      'Content-Disposition':
+          'form-data; name="file"; filename="${query!['name']}"',
+      'Content-Type': headers['Content-Type']!
+    });
+    request.files.add(await http.MultipartFile.fromPath("file", file));
+    request.headers.addAll(headers);
+    http.StreamedResponse response = await request.send();
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      return await response.stream.bytesToString();
+    }
+
+    throw InvalidHttpRequestException(
+        invalidRequest:
+            InvalidRequest(uri: uri, method: 'UPLOAD', headers: headers),
+        invalidResponse: InvalidResponse(
+            statusCode: response.statusCode,
+            body: "",
             headers: response.headers,
             reasonPhrase: response.reasonPhrase));
   }
